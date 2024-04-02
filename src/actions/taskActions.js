a20d8033294f02efce6cad5ae8a0b250c0dfe50f
Ashish040801 <ashsharma0408@gmail.com>
2024-04-02 23:12:57 +0530

    Thank You For Assignment

---
+++ src/actions/taskActions.js
@@ -0,0 +1,17 @@
+// src/actions/taskActions.js
+import { ADD_TASK, DELETE_TASK, TOGGLE_TASK } from './type';
+
+export const addTask = (task) => ({
+  type: ADD_TASK,
+  payload: task,
+});
+
+export const deleteTask = (id) => ({
+  type: DELETE_TASK,
+  payload: id,
+});
+
+export const toggleTask = (id) => ({
+  type: TOGGLE_TASK,
+  payload: id,
+});
